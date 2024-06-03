d8eb49a1935f9bd2ea1a01432314d91775adda45
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-06-02 23:59:37 -1100

    update

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { BallTriangle } from "react-loader-spinner";
 import { useParams } from "react-router-dom";
 
 const Player = () => {
@@ -56,6 +57,18 @@ const Player = () => {
       setLoading(false);
     }
   };
+  render(
+    <BallTriangle
+      height={100}
+      width={100}
+      radius={5}
+      color="#4fa94d"
+      ariaLabel="ball-triangle-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
 
   useEffect(() => {
     fetchMovieVideos();
@@ -65,7 +78,8 @@ const Player = () => {
     <>
       {loading ? (
         <div>
-          <h1>Loading...</h1>
+          {/* <h1>Loading...</h1> */}
+          <BallTriangle />
         </div>
       ) : (
         <div className=" w-full p-8 h-auto">
