e7b93b37bf050a7627f9522760d72d730fb7a19b
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-06-03 02:22:24 -1100

    update

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className=" p-8 w-full bg-black text-white h-[300px] ">
       <div className=" max-w-[500px] ">
-        <h1 className=" text-3xl font-bold text-white">
+        <h1 className=" text-3xl md:text-2xl font-bold text-white">
           Our platform is trusted by millions & features best updated movies all
           around the world.
         </h1>
