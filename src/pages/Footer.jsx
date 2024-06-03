a115246f76efe38789e69d1487e8ee9e297c37a1
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-06-03 02:21:23 -1100

    update

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className=" p-8 w-full bg-black text-white h-[300px] ">
-      <div className=" w-[500px] ">
+      <div className=" max-w-[500px] ">
         <h1 className=" text-3xl font-bold text-white">
           Our platform is trusted by millions & features best updated movies all
           around the world.
